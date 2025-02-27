07c1ed44c496e320648197d31a6c2c29b6a19646
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-02-27 16:20:41 +0100

    Styles: ObjectList container and list CSS

--- src/components/ObjectList.tsx
+++ src/components/ObjectList.tsx
@@ -4,13 +4,15 @@ export default function ObjectList() {
   const { objects } = useObjectContext();
 
   return (
-    <ul>
-      {objects.map((obj) => (
-        <li key={obj.id}>
-          <div className='object-list-item-name'>{obj.name}</div>
-          <div className='object-list-item-type'>{obj.type}</div>
-        </li>
-      ))}
-    </ul>
+    <div className='container'>
+      <ul className='object-list-items'>
+        {objects.map((obj) => (
+          <li key={obj.id} className='object-list-item'>
+            <div>{obj.name}</div>
+            <div>{obj.type}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
